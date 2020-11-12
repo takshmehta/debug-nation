6848c6dce3c20993143cc6a960864c6212d13885
taksh mehta <takshmehta111@gmail.com>
2020-11-12 18:20:54 +0530

    contact improvements

--- src/core/Contact.js
+++ src/core/Contact.js
@@ -6,6 +6,51 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function ContactUs() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name === "") {
+      return toast.error("Enter data first", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+    if (email === "") {
+      return toast.error("Enter data first", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+    if (message === "") {
+      return toast.error("Enter data first", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    } else {
+      sendEmail(e);
+      setName("");
+      setEmail("");
+      setMessage("");
+    }
+  };
   const notify = () =>
     toast.success("Message Sent", {
       position: "top-right",
@@ -18,7 +63,7 @@ export default function ContactUs() {
     });
 
   function sendEmail(e) {
-    e.preventDefault();
+    // e.preventDefault();
 
     emailjs
       .sendForm(
@@ -37,6 +82,7 @@ export default function ContactUs() {
       );
 
     e.target.reset();
+    return notify();
   }
   return (
     <div>
@@ -45,7 +91,7 @@ export default function ContactUs() {
       <div className="container-fluid mt-1">
         <div className="row">
           <div className="d-flex justify-content-center col-md-12  mt-5 ">
-            <form onSubmit={sendEmail} style={{ width: "400px" }}>
+            <form onSubmit={handleSubmit} style={{ width: "400px" }}>
               <div className="form-group">
                 <input
                   type="text"
@@ -53,6 +99,8 @@ export default function ContactUs() {
                   // id="exampleFormControlInput1"
                   placeholder="Full Name"
                   name="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
                 />
               </div>
               <div className="form-group">
@@ -62,6 +110,8 @@ export default function ContactUs() {
                   // id="exampleFormControlInput1"
                   placeholder="Email Address"
                   name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
 
@@ -72,11 +122,14 @@ export default function ContactUs() {
                   rows="3"
                   placeholder="Message"
                   name="message"
+                  value={message}
+                  onChange={(e) => {
+                    setMessage(e.target.value);
+                  }}
                 ></textarea>
               </div>
               <div className="">
                 <input
-                  onClick={notify}
                   type="submit"
                   className=" col-md-12 btn btn-success"
                   value="Send Message"
@@ -96,14 +149,9 @@ export default function ContactUs() {
               </div>
             </form>
           </div>
-          {/* <div className="col-md-6 mt-3">
-            <img src={contact} alt="" />
-          </div> */}
         </div>
       </div>
       <Footer />
     </div>
   );
 }
-
-// export default Contact;
